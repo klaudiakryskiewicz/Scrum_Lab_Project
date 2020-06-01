27baa0a362791c93453a1426565accbf5750a6de
klaudiakryskiewicz <klaudia.sandra.kryskiewicz@gmail.com>
2020-06-01 22:02:56 +0200

    1.5 pierwsze podejście

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,14 +1,21 @@
 from datetime import datetime
+from random import sample
 
 from django.shortcuts import render
 from django.views import View
+
+from jedzonko.models import Recipe
 
 
 class IndexView(View):
 
     def get(self, request):
+        recipes = Recipe.objects.order_by('?')[:3]
+        # jak nie zrobię tego fora to pokazuje mi losową nazwę, losowe składniki i losowy opis XD
+        # for recipe in recipes:
+        #     print(recipe.name, recipe.ingredients, recipe.description)
         ctx = {"actual_date": datetime.now()}
-        return render(request, "index.html", ctx)
+        return render(request, "index.html", ctx, {'recipes': recipes})
 
 class RecipeList(View):
 
