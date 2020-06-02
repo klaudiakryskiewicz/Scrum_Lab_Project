3a94f903058e92954dc6d89980c2c70035d04379
Szymon Łasak <szymonlasak2101@gmail.com>
2020-06-02 23:32:27 +0200

    2.5 dziala ale wyglada tragicznie

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -4,7 +4,7 @@ from random import sample
 from django.shortcuts import render, redirect
 from django.views import View
 
-from jedzonko.models import Recipe, Plan
+from jedzonko.models import Recipe, Plan, Recipeplan, Dayname
 
 
 class IndexView(View):
@@ -26,9 +26,19 @@ class RecipeList(View):
 class DashboardView(View):
 
     def get(self, request):
+        last_plan = Plan.objects.latest("id")
+        breakfasts = Recipeplan.objects.filter(plan_id=last_plan.id).filter(meal_name__icontains="breakfast").values_list("recipe__name", flat=True)
+        dinners = Recipeplan.objects.filter(plan_id=last_plan.id).filter(meal_name__icontains="dinner").values_list("recipe__name", flat=True)
+        suppers = Recipeplan.objects.filter(plan_id=last_plan.id).filter(meal_name__icontains="supper").values_list("recipe__name", flat=True)
+        breakfasts_ids = Recipeplan.objects.filter(plan_id=last_plan.id).filter(meal_name__icontains="breakfast").values_list("recipe_id", flat=True)
+        dinners_ids = Recipeplan.objects.filter(plan_id=last_plan.id).filter(meal_name__icontains="dinner").values_list("recipe_id", flat=True)
+        suppers_ids = Recipeplan.objects.filter(plan_id=last_plan.id).filter(meal_name__icontains="supper").values_list("recipe_id", flat=True)
+        days = Dayname.objects.all()
         recipes_num = Recipe.objects.all().count()
-        return render(request, "dashboard.html", {"recipes_num": recipes_num})
-
+        return render(request, "dashboard.html", {"recipes_num": recipes_num, "last_plan": last_plan,
+                                                  "days": days, "breakfasts": breakfasts,
+                                                  "dinners": dinners, "suppers": suppers, "breakfasts_ids": breakfasts_ids,
+                                                  "dinners_ids": dinners_ids, "suppers_ids": suppers_ids})
 
 
 class PlansList(View):
@@ -42,18 +52,18 @@ class RecipeAdd(View):
     def get(self, request):
         return render(request, "app-add-recipe.html")
 
-class AddPlan(View):
 
+class AddPlan(View):
 
     def get(self, request):
         return render(request, "app-add-schedules.html")
-    
+
     def post(self, request):
         name = request.POST['name']
         description = request.POST['description']
         if name == '' or description == '':
             komunikat = "wypełnij wszystkie pola"
-            return render(request, 'app-add-schedules.html', {'komunikat':komunikat})
+            return render(request, 'app-add-schedules.html', {'komunikat': komunikat})
         plan = Plan.objects.create(name=name, description=description)
         id = plan.id
         url = '/plan/' + str(id) + '/details'
@@ -64,4 +74,3 @@ class AddRecipeToPlan(View):
 
     def get(self, request):
         return render(request, "app-schedules-meal-recipe.html")
-
